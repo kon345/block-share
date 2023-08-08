64a9b44bfcadcb69b7f13ca64f30a08b9d573dbb
HenryLin <kon345@gmail.com>
2023-08-08 09:19:09 +0800

    [+]首頁登入VC&群組列表（無功能）

--- blockShare/blockShare/ViewController.swift
+++ blockShare/blockShare/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LoginGroupView: UIView!
+    @IBOutlet weak var GroupSelectView: UIView!
+    @IBOutlet weak var AccountInputArea: UITextField!
+    @IBOutlet weak var PasswordInputArea: UITextField!
+    @IBOutlet weak var GroupListTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        GroupSelectView.isHidden = true
     }
 
-
+    @IBAction func loginBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func createGroupBtnPressed(_ sender: Any) {
+    }
+    @IBAction func groupCodeBtnPressed(_ sender: Any) {
+    }
 }
 
---
+++ blockShare/blockShare/ViewControllers/MainPageVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainPageVC.swift
+//  blockShare
+//
+//  Created by 林裕和 on 2023/8/7.
+//
+
+import UIKit
+
+class MainPageVC: UIViewController {
+
+    @IBOutlet weak var passwordInputArea: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
