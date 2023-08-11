365b659889191e97baf73f7a3d6f07d17faf2c93
HenryLin <kon345@gmail.com>
2023-08-11 14:24:33 +0800

    [+]登入後UI顯示切換 群組列表假資料顯示

--- blockShare/blockShare/ViewControllers/MainPageVC.swift
+++ blockShare/blockShare/ViewControllers/MainPageVC.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class MainPageVC: UIViewController {
+    var data = ["1","3","4"]
+    
+    enum textMessage : String {
+        case enterCodeTitle = "群組碼"
+        case enterCodeMessage = "請輸入群組碼"
+        case send = "送出"
+        case cancel = "取消"
+    }
+    
+    enum moveConstants : CGFloat {
+        case groupSelectionNewTop = 100
+        case groupSelectionNewBottom = 200
+    }
+    
+    
     // 帳號密碼登入區塊
     @IBOutlet weak var loginGroupView: UIView!
     // 群組列表區塊
     @IBOutlet weak var groupSelectionView: UIView!
+    @IBOutlet weak var groupSelectionBtmConstraint: NSLayoutConstraint!
     // 帳號輸入框
     @IBOutlet weak var accountInputArea: UITextField!
     // 密碼輸入框
@@ -23,17 +39,40 @@ class MainPageVC: UIViewController {
         // 未登入隱藏群組列表
         groupSelectionView.isHidden = true
         
+        
+        
         // 設定輸入框delegate
         accountInputArea.delegate = self
         passwordInputArea.delegate = self
+        
+        // 設定群組列表delegate
+        groupListTableView.dataSource = self
+        groupListTableView.delegate = self
     }
     
     // 登入按鈕按下
     @IBAction func loginBtnPressed(_ sender: Any) {
+        
+        // 隱藏登入顯示群組列表
+        loginGroupView.isHidden = true
+        groupSelectionView.isHidden = false
+        // 群組列表上移
+        groupSelectionView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: moveConstants.groupSelectionNewTop.rawValue).isActive = true
+        groupSelectionBtmConstraint.constant = 200
     }
     
     // 輸入群組碼按鈕按下
     @IBAction func groupCodeBtnPressed(_ sender: Any) {
+        let enterCodeAlert = UIAlertController(title: textMessage.enterCodeTitle.rawValue, message: textMessage.enterCodeMessage.rawValue, preferredStyle: .alert)
+        let confirm = UIAlertAction(title: textMessage.send.rawValue, style: .default) { action in
+            // TODO: 確定之後送出加入群組request
+        }
+        let cancel = UIAlertAction(title: textMessage.cancel.rawValue, style: .cancel)
+        enterCodeAlert.addTextField()
+        enterCodeAlert.addAction(confirm)
+        enterCodeAlert.addAction(cancel)
+        
+        self.present(enterCodeAlert, animated: true)
     }
     
     // 創建新群組按鈕按下
@@ -82,4 +121,20 @@ extension MainPageVC: UITextFieldDelegate{
         // 結束編輯
         self.view.endEditing(true)
     }
+}
+
+extension MainPageVC: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let groupCell = groupListTableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath)
+        
+        var config = groupCell.defaultContentConfiguration()
+        config.text = data[indexPath.row]
+        groupCell.contentConfiguration = config
+        return groupCell
+    }
+ 
 }
