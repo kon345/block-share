a885674a0b177963e5d8508e4a15bf6de196caa9
HenryLin <kon345@gmail.com>
2023-09-13 16:44:38 +0800

    [+]新增DropDown, Link Preview套件，創建方塊VC

---
+++ blockShare/blockShare/ViewControllers/AddBlockVC.swift
@@ -0,0 +1,33 @@
+//
+//  AddBlockVC.swift
+//  blockShare
+//
+//  Created by 林裕和 on 2023/9/13.
+//
+
+import UIKit
+
+class AddBlockVC: UIViewController {
+
+    @IBOutlet weak var catergoryDropDown: DropDown!
+    @IBOutlet weak var URLTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func confirmBtnPressed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
